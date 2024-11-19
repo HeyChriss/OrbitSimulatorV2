49e534dad2ad4ea67c51647f6e19bfe63ddd6876
christian <u650323084@byui.edu>
2024-11-18 20:07:01 -0700

    Prototype

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,20 +1,130 @@
-// Simulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/***********************************************************************
+ * Source File:
+ *    SIMULATOR
+ * Author:
+ *    <your name here>
+ * Summary:
+ *    The main simulator class that handles the orbital simulation
+ ************************************************************************/
+#include "uiInteract.h"
+#include "uiDraw.h"
+#include "position.h"
+#include "sputnik.h"
+#include "test.h"
+using namespace std;
 
-#include <iostream>
-
-int main()
+/*********************************************
+ * SIMULATOR
+ * The main simulator class
+ *********************************************/
+class Simulator
 {
-    std::cout << "Hello World!\n";
+public:
+    // Constructor initializes the simulation
+    Simulator(Position ptUpperRight) :
+        ptUpperRight(ptUpperRight),
+        pSputnik(new Sputnik())
+    {
+        // Initialize the random stars
+        for (int i = 0; i < NUM_STARS; i++)
+        {
+            Position pt;
+            pt.setPixelsX(random(-ptUpperRight.getPixelsX(), ptUpperRight.getPixelsX()));
+            pt.setPixelsY(random(-ptUpperRight.getPixelsY(), ptUpperRight.getPixelsY()));
+            stars[i] = pt;
+            phases[i] = random(0, 255);  // Random initial phase for twinkling
+        }
+    }
+
+    // Destructor cleans up any allocated memory
+    ~Simulator()
+    {
+        delete pSputnik;
+    }
+
+    // Move everything forward one time unit
+    void update()
+    {
+        // Move Sputnik according to physics (includes orbital motion)
+        if (pSputnik && !pSputnik->isDead())
+            pSputnik->move(1.0);
+
+        // Update star phases for twinkling
+        for (int i = 0; i < NUM_STARS; i++)
+            phases[i] = (phases[i] + 1) % 256;
+    }
+
+    // Draw everything on the screen
+    void draw(ogstream& gout)
+    {
+        // Draw the stars first (background)
+        for (int i = 0; i < NUM_STARS; i++)
+            gout.drawStar(stars[i], phases[i]);
+
+        // Draw stationary Earth
+        Position posEarth;
+        gout.drawEarth(posEarth, 0.0);
+
+        // Draw Sputnik last (foreground)
+        if (pSputnik && !pSputnik->isDead())
+            pSputnik->draw(gout);
+    }
+
+private:
+    Position ptUpperRight;         // Size of the screen
+    Sputnik* pSputnik;            // The Sputnik satellite
+    static const int NUM_STARS = 100;
+    Position stars[NUM_STARS];     // Array of star positions
+    uint8_t phases[NUM_STARS];     // Array of star phases
+};
+
+/*************************************
+ * CALLBACK
+ * Handle all the frames by moving everything and drawing
+ **************************************/
+void callBack(const Interface* pUI, void* p)
+{
+    // Cast the void pointer to a simulator object
+    Simulator* pSim = (Simulator*)p;
+
+    // Update the simulation
+    pSim->update();
+
+    // Draw everything
+    Position pos;  // Center of the screen
+    ogstream gout(pos);
+    pSim->draw(gout);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*********************************
+ * Main handles command line parameters and creates the game
+ *********************************/
+#ifdef _WIN32_X
+#include <windows.h>
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ PWSTR pCmdLine,
+    _In_ int nCmdShow)
+#else // !_WIN32
+int main(int argc, char** argv)
+#endif // !_WIN32
+{
+    // Run unit tests first
+    testRunner();
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Initialize OpenGL
+    Position ptUpperRight;
+    ptUpperRight.setZoom(128000.0);
+    ptUpperRight.setPixelsX(1000.0);
+    ptUpperRight.setPixelsY(1000.0);
+
+    // Initialize the game
+    Interface ui(argc, argv, "Orbital Simulator", ptUpperRight);
+    Simulator sim(ptUpperRight);
+
+    // Play the game
+    ui.run(callBack, &sim);
+
+    return 0;
+}
